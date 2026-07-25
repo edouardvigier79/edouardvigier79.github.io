0cc7f58cdf232838878458f83e33e242c4bdb5d1
edouardvigier79 <edouardvigier@gmail.com>
2026-07-25 14:13:33 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* Service worker — cache hors-ligne + réception des photos partagées depuis la galerie */
-/* Application version 2.2 */
-const CACHE = "chantier-v12";
+/* Application version 2.3 */
+const CACHE = "chantier-v13";
 const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
