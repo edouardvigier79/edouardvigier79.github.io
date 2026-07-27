6d5180c2f68452c64291402ebceb6c77ef7f6027
edouardvigier79 <edouardvigier@gmail.com>
2026-07-27 14:53:40 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* Service worker — cache hors-ligne + réception des photos partagées depuis la galerie */
 /* Application version 3.0 (révision 2 : PCM-CAD, solde depuis constat, avant/après) */
-const CACHE = "chantier-v25";
-const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./fond-plan.jpg"];
+const CACHE = "chantier-v27";
+const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./icon-maskable.png", "./fond-plan.jpg"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
